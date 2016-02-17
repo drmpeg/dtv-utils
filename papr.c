8e128b4a8983a6129c45d7e4bcb757997208c14c
Ron Economos <w6rz@comcast.net>
2016-02-17 15:46:45 -0800

    Add copyright to PAPR calculator and update README.

--- papr.c
+++ papr.c
@@ -1,5 +1,22 @@
 /*
-PAPR calculator
+ * Copyright 2016 Ron Economos (w6rz@comcast.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+PAPR with Complementary Cumulative Distribution Function calculator
 */
 
 #include <stdio.h>
